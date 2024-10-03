3e011cc72e29548dde9a1f1fab24c6062d669fda
Dadafara <philiberaymond@gmail.com>
2024-10-03 17:16:53 +0300

    last update commit fer a londre

--- api/share-reviews.js
+++ api/share-reviews.js
@@ -242,7 +242,7 @@ async function handler(req, res) {
     <!-- Number of reviews and Company logo -->
     <g transform="translate(50, 500)">
         <text class="rating" transform="translate(0, 35)">
-            ${text_3} ${rating} / 5 | ${company.total_reviews} ${text_3}
+            ${text_3} ${rating} / 5 | ${company.reviews.length} ${text_3}
         </text>
     <g transform="translate(${svgWidth - 300}, 0)">
       <image class="logo" href="${imageBase64Logo}" height="50" width="200" />
