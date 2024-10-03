16c575ca3beb24213ef224822f3bc7b083b44813
Dadafara <philiberaymond@gmail.com>
2024-10-03 10:05:09 +0300

    add update p

--- api/share-reviews.js
+++ api/share-reviews.js
@@ -182,7 +182,7 @@ async function handler(req, res) {
   const titleY = 150;
 
   const wrappedText = wrapText(text, 27);
-  const fontPath = path.resolve(__dirname, "fonts", "Helvetica Bold.ttf");
+  const fontPath = path.resolve(process.cwd(), "fonts", "Helvetica Bold.ttf");
   const fontData = fs.readFileSync(fontPath).toString("base64");
 
   const svgImage = `
