f8f05765a437aee11bc31bf5a4159219bcacbba5
Dadafara <philiberaymond@gmail.com>
2024-10-04 07:59:36 +0300

    nnnmmlllppp

--- api/share-reviews.js
+++ api/share-reviews.js
@@ -248,14 +248,15 @@ async function handler(req, res) {
     <rect width="100%" height="100%" fill="white"/>
     
     <!-- Review text -->
-<g transform="translate(50, 100)">
+<g transform="translate(50, 100), ${leftMargin}">
   ${truncatedText
     .split("\n")
     .map(
-      (line, index) =>
+      (index) =>
         `<text class="title" x="10" y="${
           index * 36
-        }" text-anchor="start">${line}</text>`
+        }" text-anchor="start">nous avons été démarchés en porte à porte par Ector Alarm alors que nous étions chez Verisure. Ca tombait à point nommé car nous voulions revoir notre contrat. Notre prestataire n'ayant pas répondu à nos demandes, sector alarm s'est positionné avec davantage de matériel de sécurité, un prix moins élevé et une meilleure assistance. Pour le moment nous sommes très satisfaits d'être chez eux. Petit bémol : le fait de ne pas pouvoir programmer de routine sur l'application pour l'activitation du mode partiel la nuit par exemple.
+Sinon l'application est top, le matériel discret et facile d'utilisation, l'équipe commerciale très sympathiques (meme si les techniques commerciales sont les memes partout dans ce genre de secteur d'activité...) mais nous ne regrettons pas notre choix</text>`
     )
     .join("")}
 </g>
