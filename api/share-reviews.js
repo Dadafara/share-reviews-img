316d23d65105ce6e33ed4c6f228f916ba08b0162
Dadafara <philiberaymond@gmail.com>
2024-10-04 14:29:41 +0300

    bo

--- api/share-reviews.js
+++ api/share-reviews.js
@@ -177,7 +177,7 @@ async function handler(req, res) {
   const svgWidth = 1200;
   const svgHeight = 600;
   const topMargin = 100;
-  const lineHeight = 40;
+  const lineHeight = 50;
 
   const truncatedText = truncateText(review.experience, 5, 60);
 
