df6f4c880ed60acc3c39fddb6182c3d967d6d707
Dadafara <philiberaymond@gmail.com>
2024-10-04 14:37:33 +0300

    bommml

--- api/share-reviews.js
+++ api/share-reviews.js
@@ -176,7 +176,7 @@ async function handler(req, res) {
 
   const svgWidth = 1200;
   const svgHeight = 600;
-  const topMargin = 150;
+  const topMargin = 90;
   const lineHeight = 70;
 
   const truncatedText = truncateText(review.experience, 5, 60);
@@ -198,7 +198,7 @@ async function handler(req, res) {
           word-spacing: 5px;
         }
         .review-text {
-          font-size: 40px;
+          font-size: 35px;
           font-family: "Helvetica";
           text-anchor: start;
           margin-right: 100px;
