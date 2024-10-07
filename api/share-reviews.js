6d1e52da45f7035e72859791b201be607b2afe5b
Dadafara <philiberaymond@gmail.com>
2024-10-07 16:54:20 +0300

    updatellmmm

--- api/share-reviews.js
+++ api/share-reviews.js
@@ -259,7 +259,7 @@ async function handler(req, res) {
   const imageBuffer = await sharp(Buffer.from(svgImage))
     .resize(1200, 630)
     .png({ quality: 100 })
-    .withMetadata({ density: 300 })
+    .withMetadata({ density: 72 })
     .toBuffer();
   res.setHeader("Content-Type", "image/png");
   res.setHeader(
